be6cb2849bdec0c7e2c834a9cbfec55f3a45b42f
LateMigo <260859@edu.p.lodz.pl>
2025-10-07 23:32:31 +0200

    Poprawa komentarza

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int Policz (int a, int b)
 }
 int main() {
     std::cout << "Hello and welcome to \n";
-    std::cout << "W pierwszym branchu; 2 * 3 = " << Policz(2,3) << "!\n";
+    std::cout << "W master branchu (2+3)*(2-3): = " << Policz(2,3) << "!\n";
     return 0;
 }
